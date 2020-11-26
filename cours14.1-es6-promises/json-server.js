3cab5b721014345309728f23fd50106232645ca8
Julien Gascon-Samson <julien.gascon-samson@etsmtl.ca>
2020-11-26 17:47:27 -0500

    JSON server: sert des fichiers statiques

--- cours14.1-es6-promises/json-server.js
+++ cours14.1-es6-promises/json-server.js
@@ -2,9 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const staticPath = "./";
+
+const serveStatic = (req, res) => {
+    res.sendFile(req.url, {root: staticPath})
+}
+
 app.get('/', (req, res) => res.send("Hello World!"))
 
 // Static assets
+app.get('/*.html', serveStatic)
+app.get('/*.js', serveStatic)
+
+// JSON requests with timeouts
 app.get('/json1', (req,res) => setTimeout( () => res.send(JSON.stringify({ hello: "world1" })), 1000))
 app.get('/json2', (req,res) => setTimeout( () => res.send(JSON.stringify({ hello: "world2" })), 2000))
 app.get('/json3', (req,res) => setTimeout( () => res.send(JSON.stringify({ hello: "world3" })), 3000))
